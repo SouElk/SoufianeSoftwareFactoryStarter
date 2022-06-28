11989fcd190614c7bdea16207ad9ae153b5ebe0a
SouElk <soufiane.elkadouri@coppernic.fr>
2022-06-28 13:48:27 +0100

    ajout projet rh

---
+++ rh/src/test/java/com/coppernic/rh/RhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coppernic.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
